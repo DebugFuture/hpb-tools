a2c7e3c00dadd6f6b3c93a53aa34feb77cffa72a
DebugFuture <debugfuture@outlook.com>
2018-09-30 14:52:16 +0800

    update how to get local node address

--- printStatus.js
+++ printStatus.js
@@ -5,7 +5,7 @@ exec()
 
 async function exec() {
     const date = (new Date()).toISOString()
-    const address = config.nodeAddress
+    const address = await utils.query('hpb_coinbase', ['latest'])
 
     const rawBalance = await utils.query('hpb_getBalance', [address, 'latest'])
     const balance = Math.round(parseInt(rawBalance, 16) / 100000000000000) / 10000
